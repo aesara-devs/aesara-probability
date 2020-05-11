eb8e858611aeb05374208fc1c94864acae984db2
Brandon T. Willard <brandonwillard@gmail.com>
2020-05-11 15:41:30 -0500

    Update org-file build scripts

--- src/org/org-export-init.el
+++ src/org/org-export-init.el
@@ -6,13 +6,16 @@
 (add-to-list 'load-path working-dir)
 
 (setq make-backup-files nil)
+(setq load-prefer-newer t)
 
 (require 'org)
 (require 'ox)
-(require 'ob-ipython)
-(require 'projectile)
+(require 'f)
+(require 'org-ref)
+(require 'org-ref+)
+(require 'ox-latex+)
 
-(projectile-mode)
+(org-ref+-mode +1)
 
 (setq org-export-allow-bind-keywords t)
 (setq org-export-backends '(latex html))
@@ -21,53 +24,25 @@
 (add-to-list 'org-babel-load-languages '(org . t))
 (add-to-list 'org-babel-load-languages '(latex . t))
 (add-to-list 'org-babel-load-languages '(python . t))
-(add-to-list 'org-babel-load-languages '(ipython . t))
 
 (org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages)
 
 (setq org-latex-listings 'minted
-  org-latex-prefer-user-labels t
-  org-confirm-babel-evaluate nil
-  org-latex-packages-alist '(("" "minted"))
-  org-babel-default-inline-header-args '((:exports . "code") (:eval . "never") (:results . "none")))
+      org-latex-prefer-user-labels t
+      org-confirm-babel-evaluate nil
+      org-latex-packages-alist '(("" "minted"))
+      org-babel-default-inline-header-args '((:exports . "code") (:eval . "never") (:results . "none")))
 
-;;; Setup `ob-ipython'
-(setq ob-ipython-resources-dir (or (getenv "FIGURES_DIR") "../../figures"))
+(setq debug-on-error t)
+(add-to-list 'org-publish-project-alist
+             `("TeX"
+               :base-directory ,working-dir
+               :publishing-directory ,(getenv "TEX_DIR")
+               :figure-dir ,(getenv "FIG_DIR")
+               :base-extension "org"
+               :include (,(f-full (getenv "PUB_FILE")))
+               :publishing-function org-latex+-publish-to-latex))
 
-(defun ob-ipython--process-response (ret file result-type)
-  "Don't append 'Out[...]:\n' junk to value-type output!"
-  (let ((result (cdr (assoc :result ret)))
-         (output (cdr (assoc :output ret))))
-    (if (eq result-type 'output)
-      output
-      (car (->> (-map (-partial 'ob-ipython--render file)
-                  (list (cdr (assoc :value result))
-                    (cdr (assoc :display result))))
-             (remove-if-not nil))))))
+(setq org-publish-use-timestamps-flag nil)
 
-(defun ob-ipython--render (file-or-nil values)
-  "Display `value' output without prepended prompt."
-  (let ((org (lambda (value)
-               value))
-        (png (lambda (value)
-               (let ((file (or file-or-nil
-                               (ob-ipython--generate-file-name ".png"))))
-                 (ob-ipython--write-base64-string file value)
-                 (format "[[file:%s]]" file))))
-        (svg (lambda (value)
-               (let ((file (or file-or-nil
-                               (ob-ipython--generate-file-name ".svg"))))
-                 (ob-ipython--write-string-to-file file value)
-                 (format "[[file:%s]]" file))))
-        (html (lambda (value)))
-        (txt (lambda (value)
-               (when (s-present? value)
-                 (s-replace "'" "" value)))))
-    (or (-when-let (val (cdr (assoc 'text/org values)))
-          (funcall org val))
-        (-when-let (val (cdr (assoc 'image/png values)))
-          (funcall png val))
-        (-when-let (val (cdr (assoc 'image/svg+xml values)))
-          (funcall svg val))
-        (-when-let (val (cdr (assoc 'text/plain values)))
-          (funcall txt val)))))
+(pp org-publish-project-alist)
